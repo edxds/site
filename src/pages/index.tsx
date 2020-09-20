51fc39acfa3796f2b9eb078231f6d4c4980889af
Eduardo Souza <edux404@gmail.com>
2020-09-20 03:55:21 -0300

    feat: Add initial version of Landing section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
 
-import { useTranslation } from '@root/i18n';
-
-import { SwapLanguageButton } from '@src/components/SwapLanguageButton';
-import { styled } from '@src/stitches.config';
-
-const Container = styled('div', {
-  width: '100vw',
-  height: '100vh',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-});
-
-const TextContainer = styled('div', {
-  flex: 1,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-});
+import { LandingSection } from '@src/sections/LandingSection';
 
 export default function Index() {
-  const { t } = useTranslation();
-
   return (
     <div>
       <Head>
         <title>EDXDS</title>
       </Head>
-      <Container>
-        <SwapLanguageButton css={{ my: '$2' }} />
-        <TextContainer css={{ marginBottom: '4rem' }}>
-          <h1>{t('hello-world')}</h1>
-        </TextContainer>
-      </Container>
+      <LandingSection />
     </div>
   );
 }
---
+++ src/sections/LandingSection.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { Link, useTranslation } from '@root/i18n';
+
+import { Box } from '@src/components/Box';
+import { Text } from '@src/components/Text';
+import { Button } from '@src/components/Button';
+import { Section } from '@src/components/Section';
+import { SwapLanguageButton } from '@src/components/SwapLanguageButton';
+import { styled } from '@src/stitches.config';
+import wave from '@src/assets/wave.svg';
+
+const SectionContainer = styled(Section, {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+
+  padding: '$4',
+  md: {
+    padding: '$16',
+    paddingTop: '$8',
+  },
+});
+
+const WaveImg = styled('img', { width: '128px', height: 'auto' });
+
+export function LandingSection() {
+  const { t } = useTranslation();
+
+  return (
+    <SectionContainer>
+      <SwapLanguageButton />
+      <Box css={{ display: 'flex', flex: 1, fd: 'column', jc: 'center', ai: 'center', sy: '$8' }}>
+        <WaveImg src={wave} aria-hidden />
+        <Box css={{ display: 'flex', fd: 'column', ai: 'center', sy: '$2' }}>
+          {/* Wrapping text elements in divs prevent the spacing margins from being overriden */}
+          <div>
+            <Text type="landingTitle" align="center" as="h1">
+              {t('landing-title')}
+            </Text>
+          </div>
+          <div>
+            <Text type="landingText" align="center" as="p" css={{ maxWidth: 600 }}>
+              {t('landing-text')}
+            </Text>
+          </div>
+        </Box>
+      </Box>
+      <Box css={{ display: 'flex', fd: 'column', ai: 'center', sy: '$3' }}>
+        <Link href="#my-projects" passHref>
+          <Button variant="contained" color="primary" as="a">
+            {t('landing-cta')}
+          </Button>
+        </Link>
+        <Link href="#about-me" passHref>
+          <Button variant="text" as="a">
+            {t('landing-cta-alt')}
+          </Button>
+        </Link>
+      </Box>
+    </SectionContainer>
+  );
+}
